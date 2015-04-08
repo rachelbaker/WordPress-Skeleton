f5af776244b08b382158528280653157d76e32a6
Rachel Baker <rachel@rachelbaker.me>
2015-04-08 14:27:58 -0500

    Update the DB_CHARSET to match changes in 4.2.

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 // ================================================
 // You almost certainly do not want to change these
 // ================================================
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
 define( 'DB_COLLATE', '' );
 
 // ==============================================================
